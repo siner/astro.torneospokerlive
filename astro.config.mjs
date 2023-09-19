c403aed35a2c9e5a3f4edbaf32ea7b9c22ce66b4
Fran <fran.siner@gmail.com>
2023-09-19 15:40:20 +0200

    Quitado ssr de cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-
-import cloudflare from "@astrojs/cloudflare";
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  output: 'server',
-  adapter: cloudflare()
-});
+    integrations: [react(), tailwind()],
+})
